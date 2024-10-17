2e7568854c8648d37688e7a613ef9994df6160aa
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-10-17 17:54:48 +0100

    Add changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,7 @@
-import React from "react";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
 
 const Home = () => {
-  return (
-    <SafeAreaView>
-      <Text>Hello</Text>
-    </SafeAreaView>
-  );
+  return <Redirect href="/(auth)/welcome" />;
 };
 
 export default Home;
